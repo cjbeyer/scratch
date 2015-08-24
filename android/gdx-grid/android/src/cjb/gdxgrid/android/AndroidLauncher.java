5e71fbffc68b96f545ff432487265f83ecc7e025
Charles Beyer <cjbeyer@gmail.com>
2015-08-24 08:18:13 -0700

    initial example from from libgdx 1.6.5 setup app

---
+++ android/gdx-grid/android/src/cjb/gdxgrid/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package cjb.gdxgrid.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import cjb.gdxgrid.GdxGrid;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxGrid(), config);
+	}
+}
---
+++ android/gdx-grid/desktop/src/cjb/gdxgrid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cjb.gdxgrid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cjb.gdxgrid.GdxGrid;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGrid(), config);
+	}
+}
